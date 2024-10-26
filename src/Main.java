0a631b80a2479e607f0a4a28297a606142112fdc
Eugene Yemelin <evgeniy.yemelin@gmail.com>
2024-10-26 07:09:36 +0300

    fix api key

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Нам нужен ваш апи ключ, одежда и мотоцикл...\n");
-        Scanner input = new Scanner(System.in);
-        String apiKey = input.nextLine();
+        // System.out.println("Нам нужен ваш апи ключ, одежда и мотоцикл...\n");
+        // Scanner input = new Scanner(System.in);
+        // String apiKey = input.nextLine();
 
-        if (apiKey.trim().isEmpty()) {
-            System.out.println("Без ключа не полетит :(");
-            return;
-        }
+        // if (apiKey.trim().isEmpty()) {
+        //     System.out.println("Без ключа не полетит :(");
+        //     return;
+        // }
 
-        Weather weather = new Weather(apiKey, 52.37125, 4.89388);
+        Weather weather = new Weather("bfccba8d-6615-4298-96f3-f3901f8dffcb", 52.37125, 4.89388);
 
         weather.printTemperature();
         weather.printAverageTemp(DayPart.day, 3);
